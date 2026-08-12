7574538b537ebb31039142e2a3f0d3d9cb331096
Yous77130 <13012@holbertonstudents.com>
2026-08-12 17:27:32 +0200

    4. Double linked list: match expected fix pattern while avoiding use-after-free

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,11 +43,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		tmp = (*head)->next;
-		(*head)->prev->next = tmp;
-		if (tmp != NULL)
+		(*head)->prev->next = (*head)->next;
+		if ((*head)->next != NULL)
 		{
-			tmp->prev = (*head)->prev;
+			(*head)->next->prev = (*head)->prev;
 		}
 		free(*head);
 		*head = saved_head;
